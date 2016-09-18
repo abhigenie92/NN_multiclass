added stochastic gradient code and fixed cross_entropy error

NN.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 import pandas as pd
 import numpy as np
 import pdb,math
@@ -6,7 +5,7 @@ import matplotlib.pyplot as plt
 
 class NN(object):
 # initialization code
-    def __init__(self,train_data,train_labels,hidden_layer_units=100,learning_rate=0.005):
+    def __init__(self,train_data,train_labels,hidden_layer_units=100,learning_rate=0.5,mini_batch_size=100):
         #  parameter initialization
         self.train_data=train_data
         self.train_labels=train_labels
@@ -58,11 +57,10 @@ class NN(object):
         '''computes the derivative of sigmoid activation function'''
         return np.exp(-z)/((1+np.exp(-z))**2)
 
-    def back_prop(self):
-        ''' returns the derivatives for weights and biases'''
+    def back_prop(self,X,y):
+        '''takes the features as X and y as label'''
         # 2nd layer
-        delta_scores = self.forward(self.train_data) # the output of the neural network.
-                                                     # A prob. for each class M X N.
+        delta_scores = self.forward(X) # the output of the neural network.A prob. for each class M X N.
         delta_scores[range(self.num_samples),self.train_labels-1] -= 1
         backprop_error_3 = delta_scores / self.num_samples
         dw2 = np.dot(self.a2.T, backprop_error_3)
@@ -77,10 +75,30 @@ class NN(object):
 
     def update(self,dw1, dw2, db1, db2):
         '''updates the weights and biases'''
+
         self.w1 += -self.learning_rate * dw1
         self.w2 += -self.learning_rate * dw2
         self.b1 += -self.learning_rate * db1
         self.b2 += -self.learning_rate * db2
+
+    def stochastic_gradient_descent(self):
+        # not using right now
+        for i in range(self.num_samples):
+            sample=self.train_data.ix[i,:]
+            label=self.train_labels.ix[i,:]-1
+
+            delta_scores = self.forward(sample)
+            delta_scores[:,label] -= 1.0
+            backprop_error_3=delta_scores
+            dw2 = np.dot(self.a2.T, backprop_error_3)
+            db2 = np.sum(backprop_error_3, axis=0, keepdims=True)
+            #1st layer
+            delta_hidden=self.derivative_sigmoid(self.z2)
+            backprop_error_2 = np.dot(backprop_error_3, self.w2.T)*delta_hidden
+            sample=np.resize(sample.values,(sample.shape[0],1)) # convert from (16
+            dw1 = np.dot(sample, backprop_error_2)
+            db1 = np.sum(delta_hidden, axis =0, keepdims=True)
+            self.update(dw1, dw2, db1, db2)
 
 # accuracy checking and training code
     def train(self):
@@ -89,9 +107,8 @@ class NN(object):
             if (i % 1==0):
                 self.cross_entropy_and_accuracy()
                 print ("Iter: %d, loss & train_acc_per (%f,%f) " %(i, *self.cross_entropy_and_accuracy()))
-            dw1, dw2, db1, db2 = self.back_prop()
-            self.update(dw1, dw2, db1, db2)
-
+                dw1, dw2, db1, db2 = self.back_prop()
+                self.update(dw1, dw2, db1, db2)
     def cross_entropy_and_accuracy(self):
         '''
         1. computes the cross-entropy cost-function for train_data and train_labels
@@ -104,7 +121,8 @@ class NN(object):
         # cross-entropy computation
         probs=self.forward(X)
         y=y-1           # y ranges in 1-26, subtract 1 for indexing to make range 0-25
-        logs=-np.log(probs[np.arange(num_samples),y])
+        prob_arr=(probs[np.arange(self.num_samples),(y-1).T])
+        logs=-np.log(prob_arr)
         cross_entropy=np.mean(logs)
 
         # training error computation
@@ -114,7 +132,7 @@ class NN(object):
         return cross_entropy, training_error
 
 if __name__ == "__main__":
-    train_data=pd.read_csv('train_data.csv')
-    train_labels=pd.read_csv('train_labels.csv')
+    train_data=pd.read_csv('train_data.csv', header=None)
+    train_labels=pd.read_csv('train_labels.csv', header=None)
     NN_obj = NN(train_data,train_labels) #15000 x 16
     NN_obj.train()